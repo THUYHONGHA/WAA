3e6e97eb5e0fd4f6c4683b0f41942088fbf2583a
Thuy Hong Ha <thuyhong.ip@gmail.com>
2017-12-24 23:11:03 -0800

    Lab8

---
+++ Lab8/src/java/ConversionBean.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+       
+
+/**
+ *
+ * @author Hong Ha
+ */
+@ManagedBean
+@RequestScoped
+public class ConversionBean implements Converter{
+
+    /**
+     * Creates a new instance of ConversionBean
+     */
+    public ConversionBean() {
+    }
+
+    @Override
+    public Object getAsObject(FacesContext fc, UIComponent uic, String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+    }
+
+    @Override
+    public String getAsString(FacesContext fc, UIComponent uic, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
---
+++ Lab8/src/java/ValidationBean.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Hong Ha
+ */
+@ManagedBean
+@RequestScoped
+public class ValidationBean {
+
+    /**
+     * Creates a new instance of ValidationBean
+     */
+    public ValidationBean() {
+    }
+    
+}
